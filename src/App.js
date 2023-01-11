44ee206fe50aecace2a570ad65492a519f75ffe7
Matheus Weege Rosin <matheus.rosin@softdesign.com.br>
2023-01-11 00:05:18 -0300

    added login page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import { Router } from 'react-router-dom';
+import SignIn from './components/SignIn';
 
 function App() {
   return (
-    <p>Hello world</p>
+    <SignIn />
   );
 }
 
